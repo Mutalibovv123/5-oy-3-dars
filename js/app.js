35dcc5fd6275e9155fbac0150548aa8db0635359
аппле <apple@MacBook-Air-apple.local>
2024-10-24 09:25:30 +0500

    Full

---
+++ js/app.js
@@ -0,0 +1,155 @@
+// Konstruktor funksiyasi uchun
+
+// #1 
+
+// function Information (name, age, nationality) {
+//       this.name = name;
+//       this.age = age;
+//       this.nationality = nationality;
+// }
+// let mutalibov = new Information ("Mutalibov", 17, "Uzbek")
+// console.log(mutalibov);
+
+// #2
+
+// function HeightAndWeight (height, weight) {
+//     this.height = height;
+//     this.weight = weight;
+// }
+// let mutalibov = new HeightAndWeight (177, 75);
+// console.log(mutalibov);
+
+// #3
+
+// function School(name, speacialized, subject) {
+//     this.name = name;
+//     this.speacialized = speacialized;
+//     this.subject = subject;
+// }
+// let mySchool = new School ("Prezident maktabi", "Math","English")
+// console.log(mySchool);
+
+// #4
+
+// function ComputerType(type, price, battery,) {
+//     this.type = type;
+//     this.price = price;
+//     this.battery = battery;
+// }
+// let myComputer = new ComputerType ("Mackbook air M1", "600$", "95");
+// console.log(myComputer);
+
+// #5
+
+// function Education (job, skill, extraSkill, certificate, IQ,) {
+//     this.job = job; 
+//     this.skill = skill;
+//     this.extraSkill = extraSkill;
+//     this.certificate = certificate;
+//     this.IQ = IQ;
+// }
+// let mutalibov = new Education ("Teacher", "critical thinking", "Marketing", "IELTS 7", "120")
+// console.log(mutalibov);
+
+// #5
+
+// function StudyCentre(name, overallStudents, overallTeachers,) {
+//     this.name = name;
+//     this.overallStudents = overallStudents;
+//     this.overallTeachers = overallTeachers;
+// }
+// let myStudycentre = ("Wisdom-way", 200, 10);
+// console.log(myStudycentre);
+
+// #6
+
+// function PhoneType (name, price, year, sellerName) {
+//     this.name = name;
+//     this.price = price;
+//     this.year = year;
+//     this.sellerName = sellerName;
+// }
+// let mutalibov = new phoneType ("Iphone 15", "1000$", 2024, "John" );
+// console.log(mutalibov);
+
+// #7
+
+// function Fighter (typeFighter, wins, loses, knockounts) {
+//     this.typeFighter = typeFighter;
+//     this.wins = wins;
+//     this.loses = loses;
+//     this.knockounts = knockounts;
+// }
+// let fighterType = new Fighter ("Boxer", 38, 0, 29)
+// console.log(fighterType);
+
+// #8
+
+// function Businessman (networth, age, assets, network) {
+//     this.networth = networth;
+//     this.age = age; 
+//     this.assets = assets;
+//     this.networth = network;
+// }
+// let andrewTate = new Businessman ("1.1 Billion", 38, "Yacht, Bugatti, 40 Supercars, stocks, mansions in so many countries", "Real world, War room");
+// console.log(andrewTate);
+
+// #9
+
+// function Country (population, wealth, averageSalary,) {
+//     this.population = population;
+//     this.wealth = wealth;
+//     this.averageSalary = averageSalary;
+// }
+// let countryUzbekistan = new Country (38, "100 mlrd", "300$")
+// console.log(countryUzbekistan);
+
+// #10
+
+// function Community (type, members, price, owner, collaborator, courses) {
+//     this.type = type;
+//     this.members = members;
+//     this.price = price;
+//     this.owner = owner;
+//     this.collaborator = collaborator;
+//     this.courses = courses;
+// }
+// let capitalClubCommunity = new Community ("Enteprenural network", "Businessmen", "365$ per year", "Luke Belmar", "Anthony", "E-com, Dropshipping, TIk-tok ads");
+// console.log(capitalClubCommunity);
+
+// Class 
+
+// #1
+
+class User {
+    constructor (name, age, weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight
+    }
+}
+let student = new User ("mutalibov", 17, 75);
+console.log(student);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
