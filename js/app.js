320de1511034727067a1e98ddccf207b31209f59
аппле <apple@MacBook-Air-apple.local>
2024-10-24 09:28:07 +0500

    Full

--- js/app.js
+++ js/app.js
@@ -121,15 +121,181 @@
 
 // #1
 
-class User {
-    constructor (name, age, weight) {
-        this.name = name;
-        this.age = age;
-        this.weight = weight
-    }
-}
-let student = new User ("mutalibov", 17, 75);
-console.log(student);
+// class User {
+//     constructor (name, age, weight) {
+//         this.name = name;
+//         this.age = age;
+//         this.weight = weight
+//     }
+// }
+// let student = new User ("mutalibov", 17, 75);
+// console.log(student);
+
+// #2
+
+// class JobType {
+//     constructor (hours, salary, title) {
+//         this.hours = hours;
+//         this.salary = salary;
+//         this.title = title;
+//     }
+// }
+// let TeacherJob = new JobType ("9-5", "500$", "TEacher");
+// console.log(TeacherJob);
+
+// #3
+
+// class Medicine {
+//     constructor (type, price, use) {
+//         this.type = type;
+//         this.price = price;
+//         this.use = use;
+//     }
+// }
+// let medicineType = new Medicine ("Aspirin", "1$", "Headache");
+// console.log(medicineType);
+
+// #4
+
+// class Personality {
+//     constructor (type, advantage, disadvantage) {
+//         this.type  = type;
+//         this.advantage = advantage;
+//         this.disadvantage = disadvantage;
+//     }
+// }
+// let typePersonality = new Personality ("introvert", "you can become alone which is good for some people", "isolation");
+// console.log(typePersonality);
+
+// #5
+
+// class CourseType {
+//     constructor (type, price, lasts) {
+//         this.type = type;
+//         this.price = price;
+//         this.lasts = lasts;
+//     }
+// }
+// let informationAboutCourse = new courseType ("Frontend", "80$ per month", "8 months");
+// console.log(informationAboutCourse);
+
+// #6
+
+// class CarType {
+//     constructor (type, price, year, colour) {
+//         this.type = type; 
+//         this.price = price;
+//         this.year = year;
+//         this.colour = colour;
+//     }
+// }
+// let carBmw = new carType ("Bmw", "300000$", 2024, "black");
+// console.log(carBmw);
+
+// #7
+
+// class Movie {
+//     constructor (type, actors, popularity) {
+//         this.type = type;
+//         this.actors = actors;
+//         this.popularity = popularity;
+//     }
+// }
+// let AvengersMovie = new Movie ("fantastic", "John Smith", 2);
+// console.log(AvengersMovie);
+
+// #8
+
+// class AverageSalary {
+//     constructor (country, salary, job) {
+//         this.country = country;
+//         this.salary = salary;
+//         this.job = job;
+//     }
+// }
+// let salaryInForeignCountry = new AverageSalary ("Usa", "150$ per day", "IT specailist");
+// console.log(salaryInForeignCountry);
+
+// #9
+
+// class StudentInformation {
+//     constructor (grade, whichClass,) {
+//         this.grade = grade
+//         this.whichClass = whichClass
+//     }
+// }
+// let AbrorStudent = new StudentInformation (5, "10-b");
+// console.log(AbrorStudent);
+
+// #10
+
+// class BoxerResults {
+//     constructor(wins, loses, knockouts) {
+//         this.wins = wins;
+//         this.loses = loses;
+//         this.knockouts = knockouts
+//     }
+// }
+// let golovkinResults = new BoxerResults(35, 0, 35);
+// console.log(golovkinResults);
+
+// Masalalar
+
+// 1-masala
+
+// function onylAgeandNAme(massive) {
+//     forEach(function (value) {
+//         console.log(value.name);
+//         console.log(value.age);
+
+//     })
+// }
+
+// 2-masala
+
+// function increseToTwentyPercent(massive) {
+//     forEach(function(value){
+//         console.log(value.price / 20 * 100);
+//     })
+// }
+
+// 3-masala
+
+// function ifOverEighteen(arr) {
+//     let res = arr.every(function(value){
+//         return value.age >= 18
+//     })
+//     return res
+// }
+
+// 4-masala
+
+// function writingTheNamesOFAuthorsInTheSameWay(arr) {
+//     forEach(function(value){
+//     value.author = "Said Ahmad"
+//     })
+//     return arr
+// }
+
+// 5-masala
+
+// function increaseWorkersSalarytoTenPercentAndAddThemTOAnewMassive(arr) {
+//     let res = arr.map(function(value){
+//         return value.salary + (value.salary / 10 * 100)
+//     })
+//     return res
+// }
+
+// 6-masala
+
+// function addMrToTheNames(arr) {
+//     forEach(function(value){
+//         value.name = "Mr" + value.name;
+//     })
+//     return arr
+// }
+
+// 7-masala
 
 
 
@@ -150,6 +316,29 @@ console.log(student);
 
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
